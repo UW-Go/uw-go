b96f0a6a72f1b0716c9921abf603531d6e35d85b
dannywuwu <d246wu@gmail.com>
2022-10-21 13:55:44 -0400

    basic location data structure

--- server/app.py
+++ server/app.py
@@ -1,15 +1,30 @@
 from flask import Flask
+from server.locations import Locations
 
 app = Flask(__name__)
+
+# location information storage cache
+locations = Locations()
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/home')
 def home():
     return 'Home Page!'
+
+
+@app.route('/api/locations')
+def locations():
+    return locations
+
+
+@app.route('/api/route')
+def route():
+    return 'Route!'
 
 
 if __name__ == '__main__':
---
+++ server/locations.py
@@ -0,0 +1,56 @@
+class Locations:
+    '''
+    A data structure to hold collections of LocationNodes
+    ...
+    Attributes
+    -----------
+    id_nodes_map : dict<int, LocationNode>
+        maps integer location IDs to their respective LocationNodes
+
+    name_id_map : dict<string, ID>
+        maps names of locations to integer location IDs
+
+    Methods
+    -------
+    '''
+
+    def __init__(self):
+        id_nodes_map = dict()
+        name_id_map = dict()
+
+    def get_location_node(self, location_id):
+        return self.id_nodes_map.get(location_id, None)
+
+    def set_location_node(self, location_id, location_node):
+        return self.id_nodes_map.set(location_id, location_node)
+
+    def get_location_id(self, location_name):
+        return self.name_id_map.get(location_name, None)
+
+    def add_location(self, location_name):
+        if location_name in self.name_id_map:
+            # duplicate location
+            raise Exception(f'{location_name} is already a location!')
+        # map location name to ID -> ID is based off number of entries in name_id_map
+        location_id = len(self.name_id_map.keys())
+        self.name_id_map[location_name] = location_id
+        return location_id
+
+
+class LocationNode:
+    '''
+    A data structure to hold location data pertaining to key points on routes
+    ...
+    Attributes
+    -----------
+    direction: str
+    title: str
+    arrow: str
+
+    Methods
+    -------
+    '''
+    def __init__(self, direction, title, arrow):
+        self.direction = direction
+        self.title = title
+        self.arrow = arrow
